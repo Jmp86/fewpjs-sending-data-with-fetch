e3f6f2be6f955e0a3140d9fbd203de7546ebd0f3
Justin <jmp86@yahoo.com>
2021-05-12 21:23:27 -1000

    commit tree b1678192d171f91a1777fe0a954f822a9ab7e261

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+function submitData (name, email) {
+ return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email
+        })
+      })
+        .then(function(response) {
+            return response.json();
+    })
+        .then(function(object) {
+            console.log(object);
+            appendId(object.id)
+    })
+        .catch(function(error) {
+            alert("Bad things! Ragnarők!");
+            console.log(error.message);
+            appendId(error.message)
+    });
+}
+const bodyTag = document.querySelector('body')
+
+function appendId(id) {
+    const divTag = document.createElement('div')
+    divTag.innerHTML = id
+    bodyTag.appendChild(divTag)
+}
